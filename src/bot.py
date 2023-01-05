027493953cab8f1b9338466e70a7342469b67889
dlmateogarcialopez <dl.mgarcia@umanizales.edu.co>
2023-01-05 16:06:11 -0500

    Asignar mecánico automáticamente terminado #2

--- src/bot.py
+++ src/bot.py
@@ -3,7 +3,7 @@ from config import bot
 from telebot import types
 from time import sleep
 from vehiculo.registrarDatosVehiculo import Vehiculo
-######################################################### sqlalquemy
+######################################################### ###sqlalquemy
 
 # Enable saving next step handlers to file "./.handlers-saves/step.save".
 # Delay=2 means that after any change in next step handlers (e.g. calling register_next_step_handler())
